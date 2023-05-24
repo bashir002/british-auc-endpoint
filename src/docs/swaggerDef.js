fb3ddd4a599ec526c2e0e93342cbb91465d4913c
bashir002 <saliubashir46@gmail.com>
2023-05-24 12:56:47 +0100

    component created

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -4,7 +4,7 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-boilerplate-sequelize API documentation',
+    title: 'british-auc-endpoint',
     version,
     license: {
       name: 'MIT',
